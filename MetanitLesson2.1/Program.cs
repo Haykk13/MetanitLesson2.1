84b2030433847a808cd5766eef04355717f83159
Hayk <hayk.hayr3@gmail.com>
2020-06-28 02:17:10 +0400

    Add project files.

---
+++ MetanitLesson2.1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MetanitLesson2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter deposit amount: ");
+            decimal deposit = decimal.Parse(Console.ReadLine());
+            Console.Write("Enter the quantity of months: ");
+            int months = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < months; i++)
+            {
+                deposit += deposit * 0.07M;
+            }
+            Console.WriteLine($"The final amount is {deposit}");
+
+            Console.ReadLine();
+        }
+    }
+}
